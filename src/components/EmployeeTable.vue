23e9a66bad9ba0014e6c35e825337b56fad7b94a
jide_omotola <jide@reach.africa>
2020-03-10 11:52:09 +0100

    Created employee table with their social accounts

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Social accounts</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Omotola Olajide</td>
+          <td>Twitter</td>
+        </tr>
+        <tr>
+          <td>Omotola Olajide</td>
+          <td>Facebook</td>
+        </tr>
+        <tr>
+          <td>Omotola Olajide</td>
+          <td>Instagram</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'employee-table',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
