a50a575dd5ade5979aee73bcebe9b2eaa1f9ddfb
jide_omotola <jide@reach.africa>
2020-03-11 13:28:18 +0100

    functions added to the delete button

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Employees</h1>
     <employee-form @add:employee="addEmployee" />
-    <employee-table :employees="employees" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" />
   </div>
 </template>
 
@@ -44,6 +44,11 @@
         const id = lastId + 1;
         const newEmployee = {...employee, id};
         this.employees = [...this.employees, newEmployee];
+      },
+      deleteEmployee(id) {
+        this.employees = this.employees.filter(
+          employee => employee.id !== id
+        )
       }
     },
   }
